4fb6aa4c66c1873c2699255ea091b5b933cec717
noahsbored <permannoach@gmail.com>
2024-06-11 15:49:52 -0400

    hi

---
+++ student.js
@@ -0,0 +1,54 @@
+
+let studentName;
+let studentAge;
+let studentGrade;
+let attendanceStatus;
+
+
+studentName = "John Doe";
+studentAge = 15;
+studentGrade = 10;
+attendanceStatus = true;
+
+
+console.log("Student Name:", studentName);
+console.log("Student Age:", studentAge);
+console.log("Student Grade:", studentGrade);
+console.log("Attendance Status:", attendanceStatus ? "Present" : "Absent");
+
+
+
+
+
+
+
+
+
+
+let num1;
+let num2;
+
+
+num1 = 10;
+num2 = 5;
+
+
+console.log( num1 + num2);
+console.log( num1 - num2);
+console.log( num1 * num2);
+console.log( num1 / num2);
+
+
+num1 += 5;
+num2 -= 2;
+console.log( num1);
+console.log( num2);
+
+console.log( num1 === num2);
+console.log( num1 > num2);
+console.log( num1 !== num2);
+
+let isPositive = num1 > 0 && num2 > 0;
+let isEven = num1 % 2 === 0 || num2 % 2 === 0;
+console.log( isPositive);
+console.log( isEven);
